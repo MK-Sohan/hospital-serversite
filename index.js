202c890107f486dabcfa2663ee4f8e1b8452803f
Md.Mahabubul Kobir <96817149+MK-Sohan@users.noreply.github.com>
2024-01-04 23:57:16 +0600

    d

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const { ObjectId } = require("mongodb");
-// const { ObjectId } = require('mongodb');
-
 var cors = require("cors");
 app.use(cors());
 app.use(express.json());
@@ -84,6 +82,12 @@ async function run() {
       const query = {};
       const cursor = allMedicine.find(query);
       const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/allmedicine/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }; // Use 'new' keyword here
+      const result = await allMedicine.findOne(query);
       res.send(result);
     });
     //  get cart data
