0c9803fd239e03c1cde28163dd5f629e6963f417
Md.Mahabubul Kobir <96817149+MK-Sohan@users.noreply.github.com>
2024-01-08 20:11:19 +0600

    d

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,8 +9,7 @@ var cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://hospitalProject:kBfJRQkArn0bymDe@hospital.bhantxq.mongodb.net/allmedicen?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@hospital.bhantxq.mongodb.net/allmedicen?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -22,6 +23,22 @@ client
   .catch((error) => {
     console.log("MongoDB not connected");
   });
+
+function varifyJwt(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    res.status(401).send({ message: "Un authorize access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    // console.log("this is decoded", decoded);
+    next();
+  });
+}
 
 async function run() {
   try {
@@ -142,8 +159,40 @@ async function run() {
         $set: user,
       };
       const result = await userCollection.updateOne(filter, updateDoc, options);
-      res.send(result);
+      var token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "40d",
+      });
+      res.send({ result, token });
     });
+
+    // making an user admin api-------------
+    app.put("/user/admin/:email", varifyJwt, async (req, res) => {
+      const email = req.params.email;
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({
+        email: requester,
+      });
+      if (requesterAccount.role === "admin") {
+        const filter = { email: email };
+
+        const updateDoc = {
+          $set: { role: "admin" },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+
+        res.send(result);
+      } else {
+        res.status(403).send({ message: "Forbidden Accrss" });
+      }
+    });
+    app.get("/admin/:email", varifyJwt, async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      console.log(isAdmin);
+      res.send({ admin: isAdmin });
+    });
+
     app.get("/alluser", async (req, res) => {
       const query = {};
       const result = await userCollection.find(query).toArray();
@@ -162,7 +211,21 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
-    // ----------- apis end-------------------
+    // app.delete("/myappointment/:e", async (req, res) => {
+    //   const email = req.params.e;
+    //   const query = { email: email };
+    //   const result = appointmentCollection.deleteOne(query);
+    //   // const result = await cursor.toArray();
+    //   res.send(result);
+    // });
+    app.delete("/deletemyappointment/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await appointmentCollection.deleteOne(query);
+      res.send(result);
+    });
+    // ----------- apis end-------------------`
   } finally {
   }
 }
